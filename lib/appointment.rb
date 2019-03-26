4f6e48faa670fda2c031f5a7166e0a60a06c0c56
Tymeckia Kendall <tykend@gmail.com>
2019-03-26 02:23:19 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,12 +2,12 @@ class Appointment
  attr_accessor :date, :patient, :doctor
  @@all = []
  
- def initialize(date, patient, doctor)
-   @patient=patient
-   @doctor=doctor
-   @date = date
-   @@all << self
- end
+ def initialize(patient, date, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
  
  def self.all
    @@all
